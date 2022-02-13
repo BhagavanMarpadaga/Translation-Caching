44852146b88fdecb260e14bde134c4d1b1a07e82
Bhagavan <bhagavanmarpadaga@gmail.com>
2022-02-13 20:12:29 +0530

    update controller

--- controller/textTranslator_controller.js
+++ controller/textTranslator_controller.js
@@ -7,82 +7,82 @@ const myCache = new NodeCache({ stdTTL: 100000 });//keeping caching time to be 1
 //function to translate code
 module.exports.translateText = function (req, res) {
 
-   
+
     //get information from req.body
     const text = req.body.translateText;
     const from = req.body.translatefrom;
     const to = req.body.translateTo;
-    
+
     //return 400 if requested information is invalid
-    if(!text||!from||!to)
-    {
+    if (!text || !from || !to) {
         return res.status(400).json({
-            message:"Invalid input"
+            message: "Invalid input"
         })
     }
-   
+
     //if requested information is already present return it 
     if (myCache.has(text)) {
+
         let getCache = myCache.get(text).to;
-  
+
         let reqText = getCache.filter((item) => {
             if (item.to === to) {
                 return item.text;
             }
         })
-       
-        return res.status(200).json({
-            message: "success",
-            text: reqText[0]
-        })
-        
-    } //else make an api call to get information
-    else {
-        const subscriptionKey = process.env.SUBSCRIPTION_KEY;
-        const endpoint = process.env.ENDPOINT;
-        const location = process.env.LOCATION;
-
-        //make a call to rest end point 
-        //requesting text translation to hindi,kannada,tamil ,telugu apart from
-        //requested language and storing it in cache
-        axios({
-            baseURL: endpoint,
-            url: '/translate',
-            method: 'post',
-            headers: {
-                'Ocp-Apim-Subscription-Key': subscriptionKey,
-                'Ocp-Apim-Subscription-Region': location,
-                'Content-type': 'application/json',
-                'X-ClientTraceId': uuidv4().toString()
-            },
-            params: {
-                'api-version': '3.0',
-                'from': from,
-                'to': [to,'hi','kn','ta','te']
-            },
-            data: [{
-                'text': text
-            }],
-            responseType: 'json'
-        }).then(function (response) {
-            
-            let translatedText = { to: response.data[0].translations }
-           
-            //store it in cache
-            myCache.set(text, translatedText);
-
-            //return only the requested language translation
+        if (reqText.length !== 0)
             return res.status(200).json({
                 message: "success",
-                text: response.data[0].translations[0].text
+                text: reqText[0]
             })
 
-        }).catch(err => {
-            console.log('error while translating code', err);
-            return res.status(500).json({
-                message:"internal server error"
-            });
+    } //Other wise make an api call to get information
+
+    const subscriptionKey = process.env.SUBSCRIPTION_KEY;
+    const endpoint = process.env.ENDPOINT;
+    const location = process.env.LOCATION;
+
+    //make a call to rest end point 
+    //requesting text translation to hindi,kannada,tamil ,telugu apart from
+    //requested language and storing it in cache
+    axios({
+        baseURL: endpoint,
+        url: '/translate',
+        method: 'post',
+        headers: {
+            'Ocp-Apim-Subscription-Key': subscriptionKey,
+            'Ocp-Apim-Subscription-Region': location,
+            'Content-type': 'application/json',
+            'X-ClientTraceId': uuidv4().toString()
+        },
+        params: {
+            'api-version': '3.0',
+            'from': from,
+            'to': [to, 'hi', 'kn', 'ta', 'te']
+        },
+        data: [{
+            'text': text
+        }],
+        responseType: 'json'
+    }).then(function (response) {
+
+        let translatedText = { to: response.data[0].translations }
+
+        //store it in cache
+        myCache.set(text, translatedText);
+
+        //return only the requested language translation
+        return res.status(200).json({
+            message: "success",
+            text: response.data[0].translations[0].text
         })
 
-    }
+    }).catch(err => {
+        console.log('error while translating code', err);
+        return res.status(500).json({
+            message: "internal server error"
+        });
+    })
+
+
 }
